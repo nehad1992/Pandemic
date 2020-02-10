23d5002e1c115b8c4f84441c19e0d48d4d1dfccf
Krista Rutz, Steven Fleming and neha doodi <krista+steven+neha@>
2020-02-10 15:15:35 -0800

    implement outbreaks when a city has reached the cap diseaseCount of 3

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,11 +1,17 @@
-import { City } from '../src/game.js';
+import { Game } from '../src/game.js';
 
 describe("City", () => {
   jest.useFakeTimers();
-  let seattle;
+    let game;
+    let seattle;
+    let paris;
+    let tokyo;
 
   beforeEach(function() {
-    seattle = new City;
+    game = new Game;
+    seattle = game.cities[2];
+    tokyo = game.cities[0];
+    paris = game.cities[1];
   })
 
   afterEach(function() {
@@ -16,17 +22,46 @@ describe("City", () => {
     expect(seattle.diseaseCount).toEqual(0);
   })
   test("should increase Seattle diseaseCount by 1 when Seattle is infected", () => {
-    seattle.infect();
+    game.infect(2);
     expect(seattle.diseaseCount).toEqual(1);
   })
   test("should increase Seattle diseaseCount by 1 after two minutes", () => {
-    seattle.setDiseaseCount();
+    game.setDiseaseCount(2);
     jest.advanceTimersByTime(120001)
     expect(seattle.diseaseCount).toEqual(1);
   })
   test("should increase Seattle diseaseCount by 1 after every two minutes", () => {
-    seattle.setDiseaseCount();
+    game.setDiseaseCount(2);
     jest.advanceTimersByTime(240001)
     expect(seattle.diseaseCount).toEqual(2);
   })
+  test("should increase Seattle diseaseCount to 3 after 6 minutes", () => {
+    game.setDiseaseCount(2);
+    jest.advanceTimersByTime(360001)
+    expect(seattle.diseaseCount).toEqual(3);
+  })
+  test("should increase Seattle diseaseCount to 3 after 8 minutes", () => {
+    game.setDiseaseCount(2);
+    jest.advanceTimersByTime(480001)
+    expect(seattle.diseaseCount).toEqual(3);
+  })
+
+  test( "should increase Paris and Tokyo's diseaseCount by 1 when Seattle's diseaseCount is capped at three and has a new addtion to its diseaseCount", () => {
+    game.setDiseaseCount(2);
+    jest.advanceTimersByTime(480001)
+    expect(seattle.diseaseCount).toEqual(3);
+    expect(paris.diseaseCount).toEqual(1);
+    expect(tokyo.diseaseCount).toEqual(1);
+    expect(game.isGameOver).toEqual(false);
+  })
+  test( "should increase Paris and Tokyo's diseaseCount by 1 when Seattle's diseaseCount is capped at three and has a new addtion to its diseaseCount", () => {
+    game.setDiseaseCount(2);
+    jest.advanceTimersByTime(840001)
+    expect(seattle.diseaseCount).toEqual(3);
+    expect(paris.diseaseCount).toEqual(3);
+    expect(tokyo.diseaseCount).toEqual(3);
+    expect(game.getTotalDiseaseCount()).toEqual(9);
+    expect(game.isGameOver).toEqual(true);
+  })
+
 })
--- src/game.js
+++ src/game.js
@@ -13,22 +13,53 @@ export class Player {
   }
 }
 
-export class City {
+class City {
   constructor(){
     this.diseaseCount = 0;
   }
+}
 
-  infect(){
-    this.diseaseCount ++;
+export class Game {
+  constructor(){
+    let tokyo = new City;
+    let paris = new City;
+    let seattle = new City;
+    this.isGameOver = false;
+    this.cities = [tokyo, paris, seattle];
   }
 
-  setDiseaseCount(){
+  getTotalDiseaseCount(){
+    let totalDisease = 0;
+    for (let i = 0; i < this.cities.length; i++){
+      totalDisease += this.cities[i].diseaseCount;
+    }
+    return totalDisease;
+  }
+
+  infect(cityIndex){
+    
+    let totalDisease = this.getTotalDiseaseCount();
+
+    if (totalDisease >= (3 * this.cities.length)){
+      console.log("game over");
+      this.isGameOver = true;
+    } else if(this.cities[cityIndex].diseaseCount == 3){ 
+
+      for (let i =0; i < this.cities.length; i++){
+        if (i != cityIndex && this.cities[i].diseaseCount < 3) {
+          this.cities[i].diseaseCount ++;
+        }
+      }
+
+    } else{
+      this.cities[cityIndex].diseaseCount ++;
+    }
+  }
+
+  setDiseaseCount(cityIndex){
     setInterval(() => {
-      this.infect();
+      this.infect(cityIndex);
+      // allCities.infect();
     }, 120000);
   }
 }
-
-var seattle = new City;
-var neha = new Player;
-neha.treat(seattle);
